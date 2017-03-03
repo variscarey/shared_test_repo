fa357fc8c7c66c2557d4a5bdfc9d11eef0a1bec3
mandlebrot <l.lautenschlager@gmail.com>
2017-03-03 15:36:22 -0700

    added date

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,11 @@
+import datetime
+today = datetime.date.today()
 name=[]
 name.append('Varis')
+name.append('Robot Lauren')
 for i in name:
     print('Hello world, my name is '+i)
+	print(+i' is the best!')
+
+print today
 
