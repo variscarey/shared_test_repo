987c951987a84212516bdc6fbe26f46045bffcbf
Abduallah Abushahma <abduallah.abushahma@ucdenver.edu>
2017-09-22 15:11:10 -0600

    Just adding new stuff

--- hello_world.py
+++ hello_world.py
@@ -2,10 +2,10 @@ name=[]
 name.append('Varis')
 name.append('Robot Lauren')
 name.append('Lawrence and Luke')
+name.append('Billy the Bad')
 for i in name:
     print('Hello world, my name is '+i)
 print('nyah')
-
 	print(+i' is the best!')
-
 Fall 2017 Tutorial additions
+Adding another line to merge into master. 
