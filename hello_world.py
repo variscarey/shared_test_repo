95ed8af2b42c6c261a53a48a4b60a84abd986290
Abduallah Abushahma <abduallah.abushahma@ucdenver.edu>
2017-09-22 15:16:21 -0600

    Adding a new feature

--- hello_world.py
+++ hello_world.py
@@ -7,5 +7,7 @@ for i in name:
     print('Hello world, my name is '+i)
 print('nyah')
 	print(+i' is the best!')
+for 1 to 5: 
+	print('1')
 Fall 2017 Tutorial additions
 Adding another line to merge into master. 
